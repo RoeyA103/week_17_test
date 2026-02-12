71f8bed16eae9ca26c62f75ed8a9cc1faa519d57
roey_averbuch <roey6922@gmail.com>
2026-02-12 15:23:20 +0200

    fix compuse + requirement in api + docker files

--- producer/app/mongo_service.py
+++ producer/app/mongo_service.py
@@ -1,7 +1,6 @@
 from pymongo import  errors
 from mongo_connection import get_collectin 
-from rich import print as pprint
-from dotenv import load_dotenv
+
 
 def load_data():
     collection = get_collectin()
@@ -13,7 +12,7 @@ def mongo_stream(collection, batch_size=3,jump=1):
                                           {"$skip":jump},
                                           {"$limit":batch_size}]))
 
-        pprint(docs)
+        print(docs)
         print("read data from mongo successfuly")
 
         return docs
